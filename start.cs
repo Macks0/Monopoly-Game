47a237752e65294315401788e6ff9eaeb9df12dc
Macks0 <94944942+Macks0@users.noreply.github.com>
2025-02-01 22:14:34 -0600

    loop

--- start.cs
+++ start.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        for (int i = 1; i <= 10; i++)
+        for (int i = 1; i <= 1110; i++)
         {
             Console.WriteLine(i);
         }
